b9e85a566a1ee01a05d82712a3e5f29e64a4ac8d
Adam Khamutaev <khamutaev91@gmail.com>
2025-06-30 19:55:35 +0300

    SonarQube setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,20 @@
 plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.51.0"
+    id("org.sonarqube") version "6.2.0.5505"
     application
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+sonar {
+    properties {
+        property("sonar.projectKey", "AdamKham_java-project-61")
+        property("sonar.organization", "adamkham")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
 
 tasks.getByName<JavaExec>("run") {
     standardInput = System.`in`
